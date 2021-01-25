d086bbf305ca966cb25d9c701701cc0cacbda760
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-25 12:35:28 -0800

    Combined JavaScript data and operators to cmpute new values.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 20;
+const height = 10;
+const area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 6;
+const payment = 10;
+const change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 95;
+const midterm = 90;
+const final = 80;
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade', typeof grade);
+
+const firstName = 'Samuel';
+const lastName = 'Cho';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 4;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 100;
+const isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
