afc25d12d4898f91ddc6c54110105a0c52d42ced
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-29 17:54:10 -0800

    Addressed comments for initial and reverse functions.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -2,7 +2,7 @@
 function initial(array) {
   var initialArray = [];
   for (var i = 0; i < array.length - 1; i++) {
-    if (i === array.length || i === array.length - 1) {
+    if (i === array.length - 1) {
       break;
     } else {
       initialArray.push(array[i]);
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -2,11 +2,7 @@
 function reverse(array) {
   var reverseArray = [];
   for (var i = array.length - 1; i >= 0; i--) {
-    if (array.length === 0) {
-      break;
-    } else {
-      reverseArray.push(array[i]);
-    }
+    reverseArray.push(array[i]);
   }
   return reverseArray;
 }
