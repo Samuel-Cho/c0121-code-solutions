56e43e63a9b6c76ae6f4477bd846a8721a480517
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-29 17:56:47 -0800

    Missread comment for initial function. Removed the conditional per comment requests.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -2,11 +2,7 @@
 function initial(array) {
   var initialArray = [];
   for (var i = 0; i < array.length - 1; i++) {
-    if (i === array.length - 1) {
-      break;
-    } else {
-      initialArray.push(array[i]);
-    }
+    initialArray.push(array[i]);
   }
   return initialArray;
 }
