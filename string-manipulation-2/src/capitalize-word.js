26aacf714985d99c953deef5b4a8110c6082bb19
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-30 19:01:14 -0800

    Exercise in progress. Finished truncate function and started capitalizeWord function.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,10 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  var capitalizeString = '';
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === 0) {
+      capitalizeString += (word[i].toUpperCase());
+    }
+  }
+  return capitalizeString;
+}
