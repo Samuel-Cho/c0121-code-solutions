21d7b32912df0c0de8806410db0779ffd10ce7e9
Samuel Cho <samuel.ms.cho@gmail.com>
2021-02-16 21:24:53 -0800

    Worked on titleCase function.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,50 @@
 /* exported titleCase */
+
+// function titleCase(title) {
+//   var newString = '';
+//   for (var i = 0; i < title.length; i++) {
+//     if (i === 0) {
+//       newString += title[i].toUpperCase();
+//       continue;
+//     }
+//     if ((title[i - 1] === ' ') || (title[i - 1] === '-')) {
+//       newString += title[i].toUpperCase();
+//       continue;
+//     }
+//     newString += title[i];
+//   }
+//   var titleArray = newString.split(' ');
+//   var titleCaseString = '';
+//   for (var j = 0; j < titleArray.length; j++) {
+//     if (titleArray[j] === 'Javascript') {
+//       titleCaseString += 'JavaScript ';
+//       continue;
+//     }
+//     if (titleArray[j].length < 4 && j !== 0){
+//       titleCaseString += titleArray[j].toLowerCase() + ' ';
+//       continue;
+//     }
+//     titleCaseString += titleArray[j] + ' ';
+//   }
+//   return titleCaseString.slice(0, titleCaseString.length - 1);
+// }
+
+function titleCase(title) {
+  var newString = '';
+  var titleArray = title.split(' ');
+  for (var i = 0; i < titleArray.length; i++) {
+    if (titleArray[i].length < 4 && i !== 0 && newString[newString.length - 2] !== ':') {
+      newString += titleArray[i].toLowerCase();
+    } else {
+      for (var j = 0; j < titleArray[i].length; j++) {
+        if (j === 0 || titleArray[i][j - 1] === '-') {
+          newString += titleArray[i][j].toUpperCase();
+          continue;
+        }
+        newString += titleArray[i][j];
+      }
+    }
+    newString += ' ';
+  }
+  return newString.slice(0, newString.length - 1);
+}
