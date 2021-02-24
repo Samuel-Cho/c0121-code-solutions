58b8b8566ad1d3ea87040540c4a9382b385dd8f3
Samuel Cho <samuel.ms.cho@gmail.com>
2021-02-23 20:40:36 -0800

    Created loop to make deck of cards instead of manually writing cards.

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -19,216 +19,37 @@ var players = [
   }
 ];
 
-var deck = [
-  {
-    rank: 'Ace',
-    suit: 'clubs'
-  },
-  {
-    rank: 2,
-    suit: 'clubs'
-  },
-  {
-    rank: 3,
-    suit: 'clubs'
-  },
-  {
-    rank: 4,
-    suit: 'clubs'
-  },
-  {
-    rank: 5,
-    suit: 'clubs'
-  },
-  {
-    rank: 6,
-    suit: 'clubs'
-  },
-  {
-    rank: 7,
-    suit: 'clubs'
-  },
-  {
-    rank: 8,
-    suit: 'clubs'
-  },
-  {
-    rank: 9,
-    suit: 'clubs'
-  },
-  {
-    rank: 10,
-    suit: 'clubs'
-  },
-  {
-    rank: 'Jack',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Queen',
-    suit: 'clubs'
-  },
-  {
-    rank: 'King',
-    suit: 'clubs'
-  },
-  {
-    rank: 'Ace',
-    suit: 'diamonds'
-  },
-  {
-    rank: 2,
-    suit: 'diamonds'
-  },
-  {
-    rank: 3,
-    suit: 'diamonds'
-  },
-  {
-    rank: 4,
-    suit: 'diamonds'
-  },
-  {
-    rank: 5,
-    suit: 'diamonds'
-  },
-  {
-    rank: 6,
-    suit: 'diamonds'
-  },
-  {
-    rank: 7,
-    suit: 'diamonds'
-  },
-  {
-    rank: 8,
-    suit: 'diamonds'
-  },
-  {
-    rank: 9,
-    suit: 'diamonds'
-  },
-  {
-    rank: 10,
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Jack',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Queen',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'King',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'Ace',
-    suit: 'hearts'
-  },
-  {
-    rank: 2,
-    suit: 'hearts'
-  },
-  {
-    rank: 3,
-    suit: 'hearts'
-  },
-  {
-    rank: 4,
-    suit: 'hearts'
-  },
-  {
-    rank: 5,
-    suit: 'hearts'
-  },
-  {
-    rank: 6,
-    suit: 'hearts'
-  },
-  {
-    rank: 7,
-    suit: 'hearts'
-  },
-  {
-    rank: 8,
-    suit: 'hearts'
-  },
-  {
-    rank: 9,
-    suit: 'hearts'
-  },
-  {
-    rank: 10,
-    suit: 'hearts'
-  },
-  {
-    rank: 'Jack',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Queen',
-    suit: 'hearts'
-  },
-  {
-    rank: 'King',
-    suit: 'hearts'
-  },
-  {
-    rank: 'Ace',
-    suit: 'spades'
-  },
-  {
-    rank: 2,
-    suit: 'spades'
-  },
-  {
-    rank: 3,
-    suit: 'spades'
-  },
-  {
-    rank: 4,
-    suit: 'spades'
-  },
-  {
-    rank: 5,
-    suit: 'spades'
-  },
-  {
-    rank: 6,
-    suit: 'spades'
-  },
-  {
-    rank: 7,
-    suit: 'spades'
-  },
-  {
-    rank: 8,
-    suit: 'spades'
-  },
-  {
-    rank: 9,
-    suit: 'spades'
-  },
-  {
-    rank: 10,
-    suit: 'spades'
-  },
-  {
-    rank: 'Jack',
-    suit: 'spades'
-  },
-  {
-    rank: 'Queen',
-    suit: 'spades'
-  },
-  {
-    rank: 'King',
-    suit: 'spades'
+var deck = [];
+
+for (var z = 0; z < 4; z++) {
+  for (var y = 1; y < 14; y++) {
+    var card = {
+      rank: null,
+      suit: null
+    };
+    if (z === 0) {
+      card.suit = 'clubs';
+    } else if (z === 1) {
+      card.suit = 'spades';
+    } else if (z === 2) {
+      card.suit = 'hearts';
+    } else {
+      card.suit = 'diamonds';
+    }
+    if (y === 1) {
+      card.rank = 'Ace';
+    } else if (y === 11) {
+      card.rank = 'Jack';
+    } else if (y === 12) {
+      card.rank = 'Queen';
+    } else if (y === 13) {
+      card.rank = 'King';
+    } else {
+      card.rank = y;
+    }
+    deck.push(card);
   }
-];
+}
 
 var shuffledDeck = _.shuffle(deck);
 
