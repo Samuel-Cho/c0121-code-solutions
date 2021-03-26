8a527c6bc433356e1c6a4fd18221567519585252
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-03-26 01:06:20 +0000

    Created all files and started on AppDrawer component.

---
+++ react-app-drawer-component/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react',
+import ReactDOM from 'reacto-dom',
+import AppDrawer from './app-drawer';
+
+class CustomAppDrawer extends React.Component {
+  render() {
+    <AppDrawer />
+  }
+}
+
+ReactDOM.render(<CustomAppDrawer />, document.querySelector('#root'));
