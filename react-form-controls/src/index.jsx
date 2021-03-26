5227cc8d674574354975274cc433e003ef7e42a3
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-03-26 22:26:53 +0000

    Built forms with React.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Email</label>
+        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+        <button type="submit">Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.querySelector('#root'));
