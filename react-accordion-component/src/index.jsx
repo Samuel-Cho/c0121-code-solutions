c2440fdb563e266ec73fed05a0f730ee9cc2df1d
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-03-26 21:46:53 +0000

    Started creating all files for challenge.

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomAccordion extends React.Component {
+  render() {
+    return <h1>hi</h1>
+  }
+}
+
+ReactDom.render(<CustomAccordion />, document.querySelector('#root'));
