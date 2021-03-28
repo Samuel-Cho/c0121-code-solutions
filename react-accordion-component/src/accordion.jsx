8397746cbd443476103183f61cc265e3131980f2
SamuelCho1 <samuel.ms.cho@gmail.com>
2021-03-28 21:30:37 +0000

    Used React to build an expandable list of information.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -9,26 +9,46 @@ export default class Accordion extends React.Component {
 
   handleClick() {
     if (event.target.matches('.tab')) {
-      if (this.state.view !== event.target['data-view']) {
-        this.setState({ view: event.target['data-view'] });
-        const textBox = event.target.closest('.tab-text');
-
+      const targetDataView = event.target.getAttribute('data-view');
+      if (this.state.view !== targetDataView) {
+        this.setState({ view: targetDataView });
+      } else {
+        this.setState({ view: null });
       }
-
     }
   }
 
   render() {
     const languages = this.props.languages;
-    const divItems = languages.map((language) => {
-      return (
-        <div onClick={this.handleClick} className='language' key={language.id}>
-          <div className='tab' data-view={language.id} onClick={language.handleClick}>{language.name}</div>
-          <div className='hidden tab-text' data-view={language.id}>{language.text}</div>
-        </div>
-      );
-    });
-    // add divItems to state??
+    let divItems = null;
+    if (this.state.view === null) {
+      divItems = languages.map((language) => {
+        return (
+          <div onClick={this.handleClick} className='language' key={language.id}>
+            <div className='tab' data-view={language.id} onClick={language.handleClick}>{language.name}</div>
+            <div className='hidden tab-text' data-view={language.id}>{language.text}</div>
+          </div>
+        );
+      });
+    } else {
+      divItems = languages.map((language) => {
+        if (language.id === this.state.view) {
+          return (
+            <div onClick={this.handleClick} className='language' key={language.id}>
+              <div className='tab' data-view={language.id} onClick={language.handleClick}>{language.name}</div>
+              <div className='tab-text' data-view={language.id}>{language.text}</div>
+            </div>
+          );
+        } else {
+          return (
+            <div onClick={this.handleClick} className='language' key={language.id}>
+              <div className='tab' data-view={language.id} onClick={language.handleClick}>{language.name}</div>
+              <div className='hidden tab-text' data-view={language.id}>{language.text}</div>
+            </div>
+          );
+        }
+      });
+    }
     return <div className="tab-container">{divItems}</div>
   }
 }
